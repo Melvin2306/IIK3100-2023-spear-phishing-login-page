cac76fd53bfffaf457d636fb45bb60673c8ebf2b
Melvin2306 <melvin.rinkleff@gmx.de>
2025-01-22 23:01:20 +0100

    added banner for clarification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header className="bg-yellow-100 text-black text-center p-2 text-sm">
+          This page was created for an ethical hacking course and is only used
+          for educational purposes. No input is saved.
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
